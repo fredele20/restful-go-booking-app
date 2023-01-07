dbb6953be239172cafa2aaba37eda05506b79366
fredele20 <victorisholaoladele@gmail.com>
2023-01-08 00:54:14 +0100

    worked on the route

---
+++ routes/user.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"booking-app/core"
+
+	"github.com/gin-gonic/gin"
+)
+
+
+
+func UserAuthentication(incomingRoutes *gin.Engine) {
+	incomingRoutes.POST("/users/register", core.Register())
+}
